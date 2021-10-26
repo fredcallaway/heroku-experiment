32a7a16f0034f4b05c6d15b09b8d79d3c1c66b6e
fredcallaway <fredc@llaway.com>
2021-10-26 14:28:45 -0400

    python3 fetch_data

--- bin/fetch_data.py
+++ bin/fetch_data.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
+import subprocess
 import logging
-import urllib2
+import requests
+from requests.auth import HTTPBasicAuth
 import pandas as pd
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import ast
 import re
 import json
 from collections import defaultdict
+import configparser
 
 logging.basicConfig(level="INFO")
+
+import hashlib
+def hash_id(worker_id):
+    return 'w' + hashlib.md5(worker_id.encode()).hexdigest()[:7]
 
 def to_snake_case(name):
     name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
@@ -37,29 +44,8 @@ class Labeler(object):
 
     __call__ = label
 
-def add_auth(url, username, password):
-    """Add HTTP authencation for opening urls with urllib2.
 
-    Based on http://www.voidspace.org.uk/python/articles/authentication.shtml
-    """
-    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-
-    # because we have put None at the start it will always use this
-    # username/password combination for urls for which `theurl` is a super-url
-    passman.add_password(None, url, username, password)
-
-    # create the AuthHandler
-    authhandler = urllib2.HTTPBasicAuthHandler(passman)
-
-    # All calls to urllib2.urlopen will now use our handler. Make sure not to
-    # include the protocol in with the URL, or HTTPPasswordMgrWithDefaultRealm
-    # will be very confused.  You must (of course) use it when fetching the
-    # page though.
-    opener = urllib2.build_opener(authhandler)
-    urllib2.install_opener(opener)
-
-
-def fetch(site_root, filename, version, force=True):
+def fetch(site_root, filename, version, auth, force=True):
     """Download `filename` from `site_root` and save it in the
     data/human_raw/`version` data folder.
     """
@@ -73,17 +59,10 @@ def fetch(site_root, filename, version, force=True):
         return
 
     # download the data
-    try:
-        handler = urllib2.urlopen(url)
-    except IOError as err:
-        if getattr(err, 'code', None) == 401:
-            logging.error("Server authentication failed.")
-            raise err
-        else:
-            raise
-    else:
-        data = handler.read()
-        logging.info("Fetched succesfully: %s", url)
+    r = requests.get(url, auth=auth)
+    r.raise_for_status()
+    data = r.text
+    logging.info("Fetched succesfully: %s", url)
 
     # write out the data file
     if not os.path.exists(os.path.dirname(dest)):
@@ -92,60 +71,61 @@ def fetch(site_root, filename, version, force=True):
         fh.write(data)
     logging.info("Saved to '%s'", os.path.relpath(dest))
     if filename == 'questiondata':
-        df = pd.read_csv(dest, header=None)
-        n_pid = df[0].unique().shape[0]
-        logging.info('Number of participants: %s', n_pid)
+        try:
+            df = pd.read_csv(dest, header=None)
+        except pd.errors.EmptyDataError:
+            logging.info('questiondata.csv is empty')
+        else:
+            n_pid = df[0].unique().shape[0]
+            logging.info('Number of participants: %s', n_pid)
+
+    # # Anonymize PIDs
+    # df = pd.read_csv(dest, header=None)
+    # df[0] = df[0].map(hash_id)
+    # df.to_csv(dest, header=None, index=False)
 
 
 def reformat_data(version):
     data_path = 'data/human_raw/{}/'.format(version)
-    pid_labeler = Labeler()
-    identifiers = {'worker_id': [], 'assignment_id': [], 'pid': []}
+    identifiers = {'worker_id': [], 'assignment_id': [], 'wid': []}
 
     # Create participants dataframe (pdf).
     def parse_questiondata():
         qdf = pd.read_csv(data_path + 'questiondata.csv', header=None)
         for uid, df in qdf.groupby(0):
-            try:
-                row = ast.literal_eval(list(df[df[1] == 'params'][2])[0])
-            except:
-                row = {}
-            wid, aid = uid.split(':')
-            identifiers['worker_id'].append(wid)
-            identifiers['assignment_id'].append(aid)
-            identifiers['pid'].append(pid_labeler(wid))
-            row['pid'] = pid_labeler(wid)
+            worker_id, assignment_id = uid.split(':')
+            if workerid.startswith('5e14'):
+                continue  # debugging 
+            identifiers['worker_id'].append(worker_id)
+            identifiers['assignment_id'].append(assignment_id)
+            row = {'wid': hash_id(worker_id)}
+            identifiers['wid'].append(row['wid'])
 
-            completed_row = df[df[1] == 'completed']
-            if len(completed_row):
-                assert len(completed_row) == 1
-                row['completed'] = True
-                data = ast.literal_eval(completed_row[2].iloc[0])
-                for k, v in data.items():
-                    print(k, v)
-                    row[k] = v
-            else:
-                bonus_row = df[df[1] == 'final_bonus']
-                if len(bonus_row):
-                    bonus = float(list(bonus_row[2])[0])
-                    row['bonus'] = bonus
+            for key, val in df.set_index(1)[2].items():
+                if key == 'params':
+                    row.update(ast.literal_eval(val))
+                elif key == 'bonus':
                     row['completed'] = True
+                    row['bonus'] = float(val)
                 else:
-                    row['bonus'] = 0
-                    row['completed'] = False
+                    row[key] = val
             yield row
 
-    pdf = pd.DataFrame(parse_questiondata())
-    pdf['version'] = version
-    idf = pd.DataFrame(pd.DataFrame(identifiers).set_index('pid'))
-    idf.to_csv(data_path + 'identifiers.csv')
+    try:
+        pdf = pd.DataFrame(parse_questiondata())
+        pdf['version'] = version
+    except pd.errors.EmptyDataError:
+        pdf = pd.DataFrame()
+    else:
+        idf = pd.DataFrame(pd.DataFrame(identifiers).set_index('wid'))
+        idf.to_csv(data_path + 'identifiers.csv')
 
     # Create trials dataframe (tdf).
     def parse_trialdata():
         tdf = pd.read_csv(data_path + 'trialdata.csv', header=None)
         tdf = pd.DataFrame.from_records(tdf[3].apply(json.loads)).join(tdf[0])
-        wids = tdf[0].apply(lambda x: x.split(':')[0])
-        tdf['pid'] = wids.apply(pid_labeler)
+        worker_ids = tdf[0].apply(lambda x: x.split(':')[0])
+        tdf['wid'] = worker_ids.apply(hash_id)
         return tdf.drop(0, axis=1)
 
     tdf = parse_trialdata()
@@ -172,25 +152,42 @@ def reformat_data(version):
     return data
 
 def main(version, address, username, password):
-    add_auth(address, username, password)
     files = ["trialdata", "eventdata", "questiondata"]
     for filename in files:
-        fetch(address, filename, version)
+        fetch(address, filename, version, HTTPBasicAuth(username, password))
+    reformat_data(version)
 
+def bash(x):
+    return subprocess.check_output(x, shell=True).decode().strip()
 
+def get_data_url():
+    fn = '.data-url'
+    if os.path.isfile(fn) :
+        with open(fn) as f:
+            return f.read()
+    else:
+        domain = bash('heroku domains').split('\n')[1]
+        url = f'https://{domain}/data'
+        with open(fn, 'w') as f:
+            f.write(url)
+        return url
+        
 if __name__ == "__main__":
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         "version",
+        nargs="?",
         help=("Experiment version. This corresponds to the experiment_code_version "
               "parameter in the psiTurk config.txt file that was used when the "
               "data was collected."))
 
-    url = "https://ball-exp.herokuapp.com/data"
-    if url == "https://ball-exp.herokuapp.com/data":
-        print('Set the URL in this file before usage. Then delete this ')
-        exit(1)
+    c = configparser.ConfigParser()
+    c.read('config.txt')
+    sp = c['Server Parameters']
 
-    args = parser.parse_args()
-    main(args.version, url, 'user', 'pw')  # from config.txt
+    version = parser.parse_args().version
+    if version == None:
+        version = c["Task Parameters"]["experiment_code_version"]
+        print("Fetching data for current version: ", version)
+    main(version, get_data_url(), sp['login_username'], sp['login_pw'])
