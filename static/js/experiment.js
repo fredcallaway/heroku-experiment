ed99734362dfaabd99c2354e1ecf83420b6b5faf
fredcallaway <fredc@llaway.com>
2022-03-21 12:22:45 -0400

    update code

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -1,4 +1,4 @@
-const PROLIFIC_CODE = 'example'  // set this to the prolific completion code to run in prolific mode
+const PROLIFIC_CODE = '6A5FDC7A'  // set this to the prolific completion code to run in prolific mode
 
 async function initializeExperiment() {
   LOG_DEBUG('initializeExperiment');
